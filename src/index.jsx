ecbc5e7cb966b338f77d57e1ec82f74d7e1b8bfc
Prashant Kumar Singhcd <pk3076889@gmail.com>
2023-04-22 02:40:41 +0530

    Variables file fixed

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 
-import "./styles/index.scss";
+import "./assets/css/index.scss";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
